06aca3973ea9a0d8f6d2f8655a26dd9b5e0cd03a
Henry Gordon <hwgordon247@gmail.com>
2016-04-17 12:00:13 +0100

    passing welcome message test

---
+++ src/JavaNetwork.java
@@ -0,0 +1,10 @@
+/**
+ * Created by hwgordon on 17/04/2016.
+ */
+public class JavaNetwork {
+    public static void main(String[] args) {
+        System.out.print("Welcome to the Java Network\nPlease enter your name:\n");
+       
+    }
+
+}
---
+++ src/JavaNetworkTest.java
@@ -0,0 +1,54 @@
+/**
+ * Created by hwgordon on 17/04/2016.
+ */
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class JavaNetworkTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+        System.setErr(null);
+    }
+    String[] args = {};
+    JavaNetwork javaNetwork = new JavaNetwork();
+
+    @Test
+    public void welcomeMessage() {
+        javaNetwork.main(args);
+        assertEquals("Welcome to the Java Network\nPlease enter your name:\n", outContent.toString());
+    }
+
+
+
+
+//    @Test
+//    public void out() {
+//        System.out.print("kieth");
+//        assertEquals("hello", outContent.toString());
+//    }
+//
+//    @Test
+//    public void err() {
+//        System.err.print("hello again");
+//        assertEquals("hello again", errContent.toString());
+//    }
+
+}
